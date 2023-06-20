ff3ce28efba40199de3b7804a06e8dbac3e6849f
Daniel <danielkuhlmann@upcmail.nl>
2023-06-20 13:43:36 +0200

    login/reg

--- src/Header.js
+++ src/Header.js
@@ -1,34 +1,39 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
-import pizzawig from "./img/pizzawig.png"
-import home from "./img/home.png"
-import google from "./img/google.png"
+import pizzawig from "./img/pizzawig.png";
+import home from "./img/home.png";
+import google from "./img/google.png";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "./config/firebase";
 
-
 function Header() {
-    const handlelogin = async () => {
-        try {
-        const cred = await signInWithPopup(auth,googleProvider);
-        console.log(cred);
-        } catch (e){
-          console.error(e);
-        }
-      };
-
-
+  const handleLogin = async () => {
+    try {
+      const cred = await signInWithPopup(auth, googleProvider);
+      console.log(cred);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
-    <div class = "header-post"> <p id = "titlesite">Pizzamwam</p> 
-      <img src={pizzawig} class="logomain"></img>
-      <p id = "homemenu">home</p>
-      <img id = "logohome" src={home}></img>
-      <button onClick={handlelogin} id = "accmenu"><img src={google} class="logomain"></img></button>
-      <Link to="/register">
-        <button  class="login-button"><p>login</p></button><button  class="register-button"><p>register</p></button>
+    <div className="header-post">
+      <p id="titlesite">Pizzamwam</p>
+      <img src={pizzawig} className="logomain" alt="Pizza Wig" />
+      <Link to="/">
+        <img id="logohome" src={home} alt="Home" />
       </Link>
-      </div>
+      <button onClick={handleLogin} id="accmenu">
+        <img src={google} className="logomain" alt="Google" />
+      </button>
+      <Link to="/login">
+        <button className="login-button"><p>Login</p></button>
+      </Link>
+      <Link to="/register"> {/* Added Link to LoginScreen */}
+        <button className="register-button"><p>Register</p></button>
+      </Link>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterScreen from './register';
 import { createRoot } from 'react-dom/client';
+import LoginScreen from './login';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
       </Routes>
     </Router>
   </React.StrictMode>
